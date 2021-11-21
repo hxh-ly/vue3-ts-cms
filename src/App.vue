7347ff6869b99537cd0d9b251b6ef9f445cc6321
hxh <1291079011@qq.com>
2021-11-21 18:43:21 +0800

    test husky

--- src/App.vue
+++ src/App.vue
@@ -4,22 +4,31 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import { defineComponent } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
 
 export default defineComponent({
-  name: "App",
+  name: 'App',
   components: {
-    HelloWorld,
+
+
+
+
+
+    HelloWorld
   },
-});
+})
 </script>
 
 <style lang="less">
 #app {
+
   font-family: Avenir, Helvetica, Arial, sans-serif;
+
   -webkit-font-smoothing: antialiased;
+
   -moz-osx-font-smoothing: grayscale;
+
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
